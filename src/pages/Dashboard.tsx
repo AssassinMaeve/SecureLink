c53f2f271ea2da435699bb703c31a7b278caa4c1
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-05-04 16:09:29 +0530

    Not Required

--- src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/Dashboard.tsx
-import React from 'react';
-
-const Dashboard: React.FC = () => {
-  return <div className="p-4">Welcome to the Dashboard!</div>;
-};
-
-export default Dashboard;
