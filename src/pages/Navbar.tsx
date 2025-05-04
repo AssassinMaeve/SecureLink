4b28519770f631941ba0bf50e8611b7a0a3b7ed8
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-05-04 23:38:21 +0530

    Navbar

---
+++ src/pages/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { auth } from "../db/firebase"; // Ensure firebase auth is correctly linked
+import "../style/NavBar.scss"; // Ensure this is correctly linked
+
+const Navbar = () => {
+  const handleLogout = () => {
+    auth.signOut();
+    // Optionally, redirect user after logout (use history or navigate for that)
+  };
+
+  return (
+    <nav className="nav">
+      <a href="/" className="nav-logo">
+        Secure Link
+      </a>
+      <ul className="nav-links">
+        <li><a href="/home">Home</a></li>
+        <li><a href="/profile">Profile</a></li>
+        <li><a href="/#" onClick={handleLogout}>Logout</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
