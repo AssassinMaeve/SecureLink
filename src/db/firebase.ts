c0e2f8e91ab42e0cf7c71043223027b5400e4b9d
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-05-09 02:24:34 +0530

    Fire Storage

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDW9dCjmoHw-vniMsMUP2WC1jGpGe92PWY",
   authDomain: "link-50f75.firebaseapp.com",
   projectId: "link-50f75",
-  storageBucket: "link-50f75.appspot.com", // fixed here
+  storageBucket: "link-50f75", // fixed here
   messagingSenderId: "572814694330",
   appId: "1:572814694330:web:22916c24ea31e9e6b3af83",
   measurementId: "G-1GTPR8PP4R"
@@ -21,4 +21,4 @@ const analytics = getAnalytics(app);
 // Export auth to use in Login page
 export const auth = getAuth(app);
 export const db = getFirestore(app); // ✅ Firestore instance
-
+export const storage = getStorage(app, "gs://link-50f75"); // ✅ Storage instance
