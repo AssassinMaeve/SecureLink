369271ca625be1b841b25d8258cb880227120abc
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-05-10 00:55:50 +0530

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthForm from "./pages/AuthForm";
 import HomePage from "./pages/HomePage"; // Create this component
+import ProfilePage from "./pages/ProfilePage"; // Create this component
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<AuthForm />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
   );
 }
