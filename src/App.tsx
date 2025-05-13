771bcf57fbd55e05066b475a1b036c86bbe616d8
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-05-14 02:06:53 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthForm from "./pages/AuthForm";
 import HomePage from "./pages/HomePage"; // Create this component
 import ProfilePage from "./pages/ProfilePage"; // Create this component
 
 function App() {
-  
   return (
+    <BrowserRouter basename="/SecureLink">  
       <Routes>
         <Route path="/" element={<AuthForm />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
+    </BrowserRouter>
   );
 }
 
