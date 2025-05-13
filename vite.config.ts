952b3ebf37e42eaa370273181b91e5798b5b6adb
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-05-14 01:45:02 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SecureLink', // ✅ CORRECT: use just the repo name with slashes
+  base: '/SecureLink/', // ✅ CORRECT: use just the repo name with slashes
   plugins: [react()],
 })
